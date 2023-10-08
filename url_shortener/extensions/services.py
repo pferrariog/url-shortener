daf47ebcd141c275eeff58707eb146177e4f3d99
Pedro Ferrari <pdrferrari08@gmail.com>
2023-10-08 01:36:57 -0300

    fix: http exception for existent aliases

--- url_shortener/extensions/services.py
+++ url_shortener/extensions/services.py
@@ -2,6 +2,7 @@ from random import choices
 from string import ascii_letters
 from string import digits
 
+from fastapi import HTTPException
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from url_shortener.extensions.schemas import UrlExists
@@ -10,20 +11,22 @@ from url_shortener.extensions.schemas import UrlSchema
 from .database import UrlModel
 
 
-def create_url_reference_code(session: Session) -> str:
+def set_url_reference_code(session: Session, url_info: UrlSchema) -> str:
     """Create a reference code with ascii characters"""
-    reference_code = "".join(choices(ascii_letters + digits, k=6))
+    reference_code = get_reference_code_alias(url_info)
     ref_code_exists = session.scalar(select(UrlModel).where(UrlModel.reference_code == reference_code))
+    if ref_code_exists and url_info.reference_code:
+        raise HTTPException(400, detail="Url alias already exists, try another!")
     if ref_code_exists:
-        return create_url_reference_code(session)
+        return set_url_reference_code(session)
 
     return reference_code
 
 
-def set_reference_code(session: Session, url_info: UrlSchema) -> str:
+def get_reference_code_alias(url_info: UrlSchema) -> str:
     """Return reference code value based on user input data"""
     if not url_info.reference_code:
-        return create_url_reference_code(session)
+        return "".join(choices(ascii_letters + digits, k=6))
     return url_info.reference_code
 
 
@@ -32,7 +35,7 @@ def insert_url_into_db(session: Session, url_info: UrlSchema) -> UrlModel:
     url_exists = session.scalar(select(UrlModel).where(UrlModel.original_url == url_info.original_url.__str__()))
     if url_exists:
         return UrlExists(url_exists, message="Url register already exists!")
-    url_reference_code = set_reference_code(session, url_info)
+    url_reference_code = set_url_reference_code(session, url_info)
     new_register = UrlModel(original_url=url_info.original_url.__str__(), reference_code=url_reference_code)
     session.add(new_register)
     session.commit()
